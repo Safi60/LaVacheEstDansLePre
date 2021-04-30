b753c0c3aa289a7551ba263e69626c5013839d76
Safi60 <safi.hanifa06@gmail.com>
2021-04-30 15:41:51 +0200

    Calcul de l'aire

---
+++ LaVacheEstDansLePre/LaVacheEstDansLePre/CalculPre.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaVacheEstDansLePre
+{
+    class CalculPre
+    {
+        private List<Piquet> listePiquets;
+
+        public CalculPre(List<Piquet> listePiquets)
+        {
+            this.listePiquets = listePiquets;
+        }
+
+        public List<Piquet> ListePiquet
+        {
+            get { return listePiquets; }
+            set { listePiquets = value; }
+        }
+
+        public double CalculAire()
+        {
+            double abscisse1;
+            double ordonne1;
+
+            double abscisse2;
+            double ordonne2;
+
+            double m_aire = 0;
+
+            double liaisonSegment = ListePiquet.ElementAt(ListePiquet.Count - 1).Abscisse * ListePiquet.ElementAt(0).Ordonnee
+                                    - ListePiquet.ElementAt(0).Abscisse * ListePiquet.ElementAt(ListePiquet.Count - 1).Ordonnee;
+
+            for (int segment = 0; segment < listePiquets.Count - 1; segment++)
+            {
+                abscisse1 = ListePiquet.ElementAt(segment).Abscisse;
+                ordonne1 = ListePiquet.ElementAt(segment).Ordonnee;
+                abscisse2 = ListePiquet.ElementAt(segment + 1).Abscisse;
+                ordonne2 = ListePiquet.ElementAt(segment + 1).Ordonnee;
+                m_aire += (abscisse1 * ordonne2) - (abscisse2 * ordonne1);
+            }
+            m_aire += liaisonSegment;
+
+            return m_aire / 2;
+        }
+
+
+    }
+}
--- LaVacheEstDansLePre/LaVacheEstDansLePre/Program.cs
+++ LaVacheEstDansLePre/LaVacheEstDansLePre/Program.cs
@@ -10,6 +10,33 @@ namespace LaVacheEstDansLePre
     {
         static void Main(string[] args)
         {
+            CalculPre calculPre = new CalculPre(new List<Piquet>());
+
+            int nombrePiquet;
+            double abscisse;
+            double ordonne;
+            double aire;
+
+            Console.Out.WriteLine("Saisir le nombre de piquets");
+            nombrePiquet = int.Parse(Console.In.ReadLine());
+
+            for (int compteurPiquet = 0; compteurPiquet < nombrePiquet; compteurPiquet++)
+            {
+                //Saisie des coordonnée {abscisse, ordonne} de chaque piquet
+                Console.Out.WriteLine("Saisir le piquet {0}", compteurPiquet);
+                abscisse = double.Parse(Console.In.ReadLine());
+                ordonne = double.Parse(Console.In.ReadLine());
+
+                Piquet newPiquet = new Piquet(abscisse, ordonne);
+                newPiquet.Abscisse = abscisse;
+                newPiquet.Ordonnee = ordonne;
+                calculPre.ListePiquet.Add(newPiquet);
+            }
+
+            //Renvoie l'aire du polygone
+            aire = Math.Abs(calculPre.CalculAire());
+            Console.Out.WriteLine("Aire = {0}", aire);
+
         }
     }
 }
