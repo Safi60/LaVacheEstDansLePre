cdecbe6365f5b91ab190b8fdb67b89410238e475
Safi60 <safi.hanifa06@gmail.com>
2021-04-30 15:38:20 +0200

    Ajout de la classe Piquet

---
+++ LaVacheEstDansLePre/LaVacheEstDansLePre/Piquet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaVacheEstDansLePre
+{
+    public class Piquet
+    {
+        private double m_abscisse;
+        private double m_ordonnee;
+
+        public double Abscisse
+        {
+            get { return m_abscisse; }
+            set { m_abscisse = value; }
+        }
+
+        public double Ordonnee
+        {
+            get { return m_ordonnee; }
+            set { m_ordonnee = value; }
+        }
+
+        /// Constructeur de la classe Piquet
+        public Piquet(double p_abscisse, double p_ordonnee)
+        {
+            this.m_abscisse = p_abscisse;
+            this.m_ordonnee = p_ordonnee;
+        }
+    }
+}
